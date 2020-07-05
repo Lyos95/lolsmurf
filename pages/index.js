4ca96cbf7ab8a64e651cd604a8d722cde75f58c9
Francisco <fran_uma@outlook.es>
2020-07-05 13:29:40 +0200

    revert

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,14 @@
 import React, { Component } from 'react';
-import dynamic from 'next/dynamic'
-
-const Navbar = dynamic(import('../components/Layout/Navbar'))
-const Footer = dynamic(import('../components/Layout/Footer'))
-const Banner = dynamic(import('../components/shop-style-four/Banner'))
-const Facility = dynamic(import('../components/shop-style-four/Facility'))
-const OfferArea = dynamic(import('../components/shop-style-four/OfferArea'))
-const Products = dynamic(import('../components/shop-style-one/Products'))
-const ArticleContent = dynamic(import('../components/Common/ArticleContent'))
-
-
-//import ArticleContent from '../components/Common/ArticleContent';
-
-//import Navbar from '../components/Layout/Navbar';
-//import Footer from '../components/Layout/Footer';
-/*
+import Navbar from '../components/Layout/Navbar';
+import Footer from '../components/Layout/Footer';
 import Banner from '../components/shop-style-four/Banner';
 import Facility from '../components/shop-style-four/Facility';
 import OfferArea from '../components/shop-style-four/OfferArea';
 import Products from '../components/shop-style-one/Products';
-*/
-
 import { connect } from 'react-redux';
 import {checkIfWeHaveThatAmountOfAccs} from '../store/actions/accountsActions'
 import { ToastContainer, toast, Slide } from 'react-toastify';
-
+import ArticleContent from '../components/Common/ArticleContent';
 import {NextSeo} from 'next-seo'
 //import Pixel from '../components/Pixel'
 class Index extends Component {
