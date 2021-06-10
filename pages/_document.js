f44f0a9587b1367ac2dd62a346f52555b7cc62cd
unknown <lyosprogramming@gmail.com>
2021-06-10 19:23:38 +0200

    Light house improvements

--- pages/_document.js
+++ pages/_document.js
@@ -14,10 +14,7 @@ class MyDocument extends Document {
                 </Head>
                 <body>
                     <Main />
-                    <NextScript />                   
-                    <script
-                        src="https://www.paypal.com/sdk/js?client-id=ActeDxf8liyWa8og-xYgIJs5G_JiB70Zk_w1_Xf-M-UosC8j7Eq8V4_Imd5us934RPmWJf6LXPtty3xE&components=buttons,funding-eligibility">
-                    </script>
+                    <NextScript /> 
                 </body>
             </Html>
         )
