7fe6a66370475d4acc171067c43d02fa44d1dbe9
unknown <lyosprogramming@gmail.com>
2021-06-11 22:49:03 +0200

    font removed

--- components/Layout/Navbar.js
+++ components/Layout/Navbar.js
@@ -16,7 +16,7 @@ class Navbar extends Component {
     render() {
         return (
             <React.Fragment>
-               
+                <TopHeader />
                 <MegaMenu />
             </React.Fragment>
         );
