8eabbb6a0c763d81a240b5424e976d5170ff9a48
unknown <lyosprogramming@gmail.com>
2021-06-12 17:10:05 +0200

    LOLSMURF google removed

--- components/Layout/Navbar.js
+++ components/Layout/Navbar.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
 import TopHeader from './TopHeader';
 import MegaMenu from './MegaMenu';
-import { initGA, logPageView } from "../googleAnalytics.js"
+//import { initGA, logPageView } from "../googleAnalytics.js"
 
 class Navbar extends Component {
 
     componentDidMount () {
-        if (!window.GA_INITIALIZED) {
+       /* if (!window.GA_INITIALIZED) {
           initGA()
           window.GA_INITIALIZED = true
         }
-        logPageView()
+        logPageView()*/
       }
       
     render() {
